ae945c22fb22456f49c598e2288847c08430ff40
Nick Merc <nmercadante@zagmail.gonzaga.edu>
2020-04-08 16:55:15 -0700

    Initial Commit

---
+++ Take Home Midterm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Take Home Midterm
+//
+//  Created by Nicholas Mercadante on 4/8/20.
+//  Copyright © 2020 Nicholas Mercadante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
