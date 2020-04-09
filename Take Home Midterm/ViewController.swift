81a4d7bf8fca336e3e45e421c83ed912dedaffff
Nick Merc <nmercadante@zagmail.gonzaga.edu>
2020-04-08 19:58:12 -0700

    Alright this one actually works

--- Take Home Midterm/ViewController.swift
+++ Take Home Midterm/ViewController.swift
@@ -27,16 +27,36 @@ class ViewController: UIViewController {
     
     var candidate = ["Warren", "Sanders", "Klobuchar", "Buttigeg", "Bloomberg", "Biden"]
     
-    var voteCount = [0,1,2,3,4,5]
+    var voteCount = [0,0,0,0,0,0]
     
     @IBAction func stepperChanged(_ sender: UIStepper) {
-        voteCount[0] = Int((sender.value))
-            label0.text = "Warren: \(voteCount[0])"
-            label1.text = "Sanders: \(voteCount[1])"
-            label2.text = "Klobuchar: \(voteCount[2])"
-            label3.text = "Buttigeg: \(voteCount[3])"
-            label4.text = "Bloomberg: \(voteCount[4])"
-            label5.text = "Biden: \(voteCount[5])"
+            if sender.tag == 0 {
+                voteCount[0] = Int(sender.value)
+                label0.text = "Warren:  " + String(sender.value)
+            }
+            if sender.tag == 1 {
+                voteCount[1] = Int(sender.value)
+                label1.text = "Sanders:  " + String(sender.value)
+            }
+            if sender.tag == 2 {
+                voteCount[2] = Int(sender.value)
+                label2.text = "Klobuchar:  " + String(sender.value)
+            }
+            if sender.tag == 3 {
+                voteCount[3] = Int(sender.value)
+                label3.text = "Buttigeg:  " + String(sender.value)
+            }
+            if sender.tag == 4 {
+                voteCount[4] = Int(sender.value)
+                label4.text = "Bloomberg:  " + String(sender.value)
+            }
+            if sender.tag == 5 {
+                voteCount[5] = Int(sender.value)
+                label5.text = "Biden:  " + String(sender.value)
+
+                
+            }
+     
        
         
     }
@@ -46,6 +66,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
              
+                label0.text = "Warren: \(voteCount[0])"
+                label1.text = "Sanders: \(voteCount[1])"
+                label2.text = "Klobuchar: \(voteCount[2])"
+                label3.text = "Buttigeg: \(voteCount[3])"
+                label4.text = "Bloomberg: \(voteCount[4])"
+                label5.text = "Biden: \(voteCount[5])"
               
               imageView0.image = UIImage(named: "Warren")
               imageView1.image = UIImage(named: "Sanders")
