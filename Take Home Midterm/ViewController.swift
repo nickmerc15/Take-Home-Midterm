f93b3f1ece1fce35ada8f6fe43e967748931e096
Nick Merc <nmercadante@zagmail.gonzaga.edu>
2020-04-08 17:06:29 -0700

    version 1

--- Take Home Midterm/ViewController.swift
+++ Take Home Midterm/ViewController.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label0: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    
+    @IBOutlet weak var imageView0: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    
+    var candidate = ["Warren", "Sanders", "Klobuchar", "Buttigeg", "Bloomberg", "Biden"]
+    
+    var voteCount = [0,0,0,0,0,0]
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        label0.text = "Warren: \(voteCount[0])"
+              label1.text = "Sanders: \(voteCount[1])"
+              label2.text = "Klobuchar: \(voteCount[2])"
+              label3.text = "Buttigeg: \(voteCount[3])"
+              label4.text = "Bloomberg: \(voteCount[4])"
+              label5.text = "Biden: \(voteCount[5])"
+              
+              imageView0.image = UIImage(named: "Warren")
+              imageView1.image = UIImage(named: "Sanders")
+              imageView2.image = UIImage(named: "Klobuchar")
+              imageView3.image = UIImage(named: "Buttigeg")
+              imageView4.image = UIImage(named: "Bloomberg")
+              imageView5.image = UIImage(named: "Biden")
+              
+        
+        
     }
 
 
